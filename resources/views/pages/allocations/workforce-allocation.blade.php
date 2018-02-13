4b610e593c794037c52d738c0ffc9453615f8afb
Nailson <nailson@gmail.com>
2018-02-13 21:38:13 -0200

    Layout da alocação de mão de obra

--- resources/views/pages/allocations/workforce-allocation.blade.php
+++ resources/views/pages/allocations/workforce-allocation.blade.php
@@ -1,124 +1,94 @@
 @extends('main')
 @section('content')
+
 <body>
-  <div class="container-fluid">
-  	<div class="row">
-  		<div class="col-md-2">
-  			<!-- <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" /> -->
-        <img alt="Bootstrap Image Preview" src="/images/avatar.png" />
-  		</div>
-  		<div class="col-md-10">
-
-        <form class="form-horizontal" action="{{ url('create-workforce')}}" method="post" enctype="multipart/form-data">
-          {!! csrf_field() !!}
-        <fieldset>
-
-          <!-- Form Name -->
-          <legend>Alocação de Mão de Obra</legend>
-
-          <!-- Select -->
-          <div class="form-group">
-            <label class="col-md-1 control-label" for="year">OS</label>
-            <div class="col-md-3">
-              <select id="year" name="year" class="form-control" onchange="createSONumber()">
-                <option value="">-- selecione --</option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-              </select>
+    <form class="form-horizontal" action="{{ url('create-workforce')}}" method="post" enctype="multipart/form-data">
+        {!! csrf_field() !!}
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">
+                    <img alt="Bootstrap Image Preview" src="/images/avatar.png" />
+                </div>
+                <div class="col-md-10">
+                    <legend>Alocação de mão de obra</legend>
+                    <div class="row">
+                        <label for="os" class="col-md-1 control-label">OS</label>
+                        <div class="col-md-3">
+                            <select id="os" name="os" class="form-control">
+                                <option value="">-- selecione --</option>
+                                <option value="2016">2016</option>
+                                <option value="2017">2017</option>
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                            </select>
+                        </div>
+                        <label class="col-md-1 control-label" for="name">Nome</label>
+                        <div class="col-md-3">
+                            <select id="name" name="name" class="form-control">
+                                <option value="">-- selecione --</option>
+                                <option value="2016">2016</option>
+                                <option value="2017">2017</option>
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                            </select>
+                        </div>
+                        <label class="col-md-1 control-label" for="service">Serviço</label>
+                        <div class="col-md-3">
+                            <select id="service" name="service" class="form-control">
+                                <option value="">-- selecione --</option>
+                                <option value="2016">2016</option>
+                                <option value="2017">2017</option>
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <legend>Período de alocação</legend>
+                            <div class="row">
+                                <label class="col-md-2 control-label" for="start">Início</label>
+                                <div class="col-md-4">
+                                    <input id="start" name="start" type="text" placeholder="Início" class="form-control input-md" required="">
+                                </div>
+                                <label class="col-md-2 control-label" for="end">Fim</label>
+                                <div class="col-md-4">
+                                    <input id="end" name="end" type="text" placeholder="Fim" class="form-control input-md" required="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <legend>Incluir</legend>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" value="">sábados</label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" value="">domingos</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" value="">feriados</label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" value="">dias ponte</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-8">
+                        </div>
+                    </div>
+                </div>
             </div>
-
-           <label class="col-md-1 control-label" for="year">Nome</label>
-            <div class="col-md-3">
-              <select id="year" name="year" class="form-control" onchange="createSONumber()">
-                <option value="">-- selecione --</option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-              </select>
-            </div>
-
-           <label class="col-md-1 control-label" for="year">Serviço</label>
-            <div class="col-md-3">
-              <select id="year" name="year" class="form-control" onchange="createSONumber()">
-                <option value="">-- selecione --</option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-              </select>
-            </div>
-          </div>
-
-
-
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="fullName">Nome completo</label>
-            <div class="col-md-10">
-            <input id="fullName" name="fullName" type="text" placeholder="Nome completo" class="form-control input-md" required="" oninvalid="this.setCustomValidity('Insira o nome do colaborador.')" oninput="setCustomValidity('')">
-
-            </div>
-          </div>
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="name">Identificação</label>
-            <div class="col-md-5">
-            <input id="name" name="name" type="text" placeholder="Nome ou apelido" class="form-control input-md" required="" data-error="deu merda">
-
-            </div>
-          </div>
-
-          <!-- Select Basic -->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="contract">Contrato</label>
-            <div class="col-md-5">
-              <select id="contract" name="contract" class="form-control">
-                <option value="1">AT</option>
-                <option value="2">CONE</option>
-                <option value="3">MEI</option>
-                <option value="4">WR</option>
-              </select>
-            </div>
-          </div>
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="mainPhone">Telefone 1</label>
-            <div class="col-md-5">
-            <input id="mainPhone" name="mainPhone" type="text" placeholder="Telefone principal" class="form-control input-md" required="">
-
-            </div>
-          </div>
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="alternativePhone">Telefone 2</label>
-            <div class="col-md-5">
-            <input id="alternativePhone" name="alternativePhone" type="text" placeholder="Telefone secundário" class="form-control input-md">
-
-            </div>
-          </div>
-
-          <!-- Button (Double) -->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="submit"></label>
-            <div class="col-md-5">
-              <button id="submit" name="submit" class="btn btn-primary" type="submit">Salvar</button>
-              <button id="cancel" name="cancel" class="btn btn-danger" type="button" onclick="window.location='{{ URL::previous() }}'">Cancelar</button>
-            </div>
-          </div>
-
-        </fieldset>
-        </form>
-
-
-  		</div>
-  	</div>
-  </div>
+        </div>
+    </form>
 </body>
+
 @endsection
--- resources/views/pages/service_orders/service_order.blade.php
+++ resources/views/pages/service_orders/service_order.blade.php
@@ -11,11 +11,11 @@
     if (yearField.value == "" || monthField.value == "") {
       soField.value = null;
       document.getElementById("submit").disabled = true;
-    } else {
+  } else {
       document.getElementById("submit").disabled = false;
       soField.value = "OS " + yearField.value + monthField.value;  
-    }
   }
+}
 
 </script>
 <body>
@@ -24,30 +24,30 @@
       <div class="col-md-2">
         <!-- <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" /> -->
         <img alt="Bootstrap Image Preview" src="/images/service-140x140.png" />
-      </div>
-      {{-- <div class="col-md-10"> --}}
+    </div>
+    {{-- <div class="col-md-10"> --}}
 
         <form class="form-horizontal" action="{{ url('create-service_order')}}" method="post" enctype="multipart/form-data">
           {!! csrf_field() !!}
-        <fieldset>
+          <fieldset>
 
-          <!-- Form Name -->
-          <legend>Cadastro de Ordens de Serviços</legend>
+              <!-- Form Name -->
+              <legend>Cadastro de Ordens de Serviços</legend>
 
-          <!-- Select -->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="year">Ano</label>
-            <div class="col-md-4">
-              <select id="year" name="year" class="form-control" onchange="createSONumber()">
-                <option value="">-- selecione --</option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-              </select>
+              <!-- Select -->
+              <div class="form-group">
+                <label class="col-md-2 control-label" for="year">Ano</label>
+                <div class="col-md-4">
+                  <select id="year" name="year" class="form-control" onchange="createSONumber()">
+                    <option value="">-- selecione --</option>
+                    <option value="2016">2016</option>
+                    <option value="2017">2017</option>
+                    <option value="2018">2018</option>
+                    <option value="2019">2019</option>
+                </select>
             </div>
             
-             <label class="col-md-2 control-label" for="month">Mês</label>
+            <label class="col-md-2 control-label" for="month">Mês</label>
             <div class="col-md-4">
               <select id="month" name="month" class="form-control" onchange="createSONumber()">
                 <option value="">-- selecione --</option>
@@ -63,73 +63,73 @@
                 <option value="10">Outubro</option>
                 <option value="11">Novembro</option>
                 <option value="12">Dezembro</option>
-              </select>
-            </div>
+            </select>
+        </div>
 
-          </div>
+    </div>
 
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="service_order">Número da OS</label>
-            <div class="col-md-4">
+    <!-- Text input-->
+    <div class="form-group">
+        <label class="col-md-2 control-label" for="service_order">Número da OS</label>
+        <div class="col-md-4">
             <input id="service_order" name="service_order" type="text" class="form-control input-md" placeholder="selecione ano e mês">
-            </div>
-          </div>
+        </div>
+    </div>
 
-          <!-- Button (Double) -->
-          <div class="form-group">
-            <label class="col-md-2 control-label" for="submit"></label>
-            <div class="col-md-5">
-              <button id="submit" name="submit" class="btn btn-primary" type="submit" disabled="true">Salvar</button>
-              <button id="cancel" name="cancel" class="btn btn-danger" type="button" onclick="window.location='{{ URL::previous() }}'">Cancelar</button>
-            </div>
-          </div>
+    <!-- Button (Double) -->
+    <div class="form-group">
+        <label class="col-md-2 control-label" for="submit"></label>
+        <div class="col-md-5">
+          <button id="submit" name="submit" class="btn btn-primary" type="submit" disabled="true">Salvar</button>
+          <button id="cancel" name="cancel" class="btn btn-danger" type="button" onclick="window.location='{{ URL::previous() }}'">Cancelar</button>
+      </div>
+  </div>
 
-        </fieldset>
-        </form>
-        <hr>
-        
-        <!-- Tabela com as OSs abertas -->
-        <table width="100%" border="1px">
-          <tr>
-            <th style="text-align: center;">COD</th>
-            <th style="text-align: center;">SERVIÇO</th>
-            <th style="text-align: center;">STATUS</th>
-            <th colspan="6" style="text-align: center;">Detalhes</th>
-            <th colspan="2" style="text-align: center;">Planilha</th>
-          </tr>
+</fieldset>
+</form>
+<hr>
+
+<!-- Tabela com as OSs abertas -->
+<table width="100%" border="1px">
+  <tr>
+    <th style="text-align: center;">COD</th>
+    <th style="text-align: center;">SERVIÇO</th>
+    <th style="text-align: center;">STATUS</th>
+    <th colspan="6" style="text-align: center;">Detalhes</th>
+    <th colspan="2" style="text-align: center;">Planilha</th>
+</tr>
 
 @if ( ! empty($services))
 @foreach($services as $service)
-          <!-- <tbody style="border-radius: 10px; display: inline-flex; background-color: #c0c0c0"> -->
-            <tr style="background-color: #FFFFFF">
-              <td rowspan="2" style="text-align: center;">{{ $service->cod }}</td>
-              <td rowspan="2">ADMINISTRAÇÃO</td>
-              <td rowspan="2" style="text-align: center;">{{ $service->active }}</td>
-              <td>Solicitante</td>
-              <td>{{ $service->requirer }}</td>
-              <td>Planilha para</td>
-              <td>{{ $service->spreadsheet_to }}</td>
-              <td>MO</td>
-              <td>{{ $service->mo }}</td>
-              <td rowspan="2" style="text-align: center;">x</td>
-              <td rowspan="2" style="text-align: center;">x</td>
-            </tr>
-            <tr style="background-color: #FFFFFF">
-              <td>E-mail</td>
-              <td>{{ $service->email }}</td>
-              <td>Início</td>
-              <td>{{ $service->start }}</td>
-              <td>Fim</td>
-              <td>{{ $service->end }}</td>
-            </tr>
-          <!-- </tbody> -->
-@endforeach
-@endif
-        </table>
+<!-- <tbody style="border-radius: 10px; display: inline-flex; background-color: #c0c0c0"> -->
+    <tr style="background-color: #FFFFFF">
+      <td rowspan="2" style="text-align: center;">{{ $service->cod }}</td>
+      <td rowspan="2">ADMINISTRAÇÃO</td>
+      <td rowspan="2" style="text-align: center;">{{ $service->active }}</td>
+      <td>Solicitante</td>
+      <td>{{ $service->requirer }}</td>
+      <td>Planilha para</td>
+      <td>{{ $service->spreadsheet_to }}</td>
+      <td>MO</td>
+      <td>{{ $service->mo }}</td>
+      <td rowspan="2" style="text-align: center;">x</td>
+      <td rowspan="2" style="text-align: center;">x</td>
+  </tr>
+  <tr style="background-color: #FFFFFF">
+      <td>E-mail</td>
+      <td>{{ $service->email }}</td>
+      <td>Início</td>
+      <td>{{ $service->start }}</td>
+      <td>Fim</td>
+      <td>{{ $service->end }}</td>
+  </tr>
+  <!-- </tbody> -->
+  @endforeach
+  @endif
+</table>
 
-      </div>
-    </div>
-  </div>
+</div>
+</div>
+</div>
 </body>
 @endsection
